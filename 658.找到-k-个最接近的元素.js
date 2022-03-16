4ff39d613aa442a4b03ac30a2122338d0de11196
wv1344 <wv1344@qq.com>
2022-03-16 08:09:12 +0800

    leetcode

--- 658.找到-k-个最接近的元素.js
+++ 658.找到-k-个最接近的元素.js
@@ -11,35 +11,57 @@
  * @param {number} x
  * @return {number[]}
  */
- var findClosestElements = function(arr, k, x) {
+var findClosestElements = function (arr, k, x) {
   let l = 0
-  let r = arr.length-1
+  let r = arr.length - 1
   let cur = undefined
-  if(arr[0] > x){
-    return arr.slice(0,k)
-  }else if(arr[arr.length-1]<x){
-    return arr.slice(-k)
+  let ans =  undefined
+  if (arr[0] >= x) {
+    ans =  arr.slice(0, k)
   }
-  while(l<=r){
-    let m = l + Math.floor((r-l)/2)
-    if(arr[m] === x){
+  if (arr[arr.length - 1] <= x) {
+    ans =  arr.slice(-k)
+  }
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2)
+    if (arr[m] === x) {
       cur = m
       break
-    } else if(arr[m] > x){
-      r = m-1
+    } else if (arr[m] < x) {
+      l = m
     } else {
-      l = m+1
+      r = m
     }
   }
-  console.log(cur);
-  if(cur){
 
+  let start = 0;
+  if (l + 1 - k > 0) {
+    start = l + 1 - k;
   }
 
+  while(myAbsComp(arr[start], arr[start + k],x)){
+    start++;
+    if (start + k == arr.length) break;
+  }
+
+  ans =[start, start + k];
+
+  return ans
 
 };
+var myAbsComp = function (a, b, m) {
+  // a is left, b is right a <= target <= b
+  //is |a-m| < |b-m| or (|a-m| == |b-m| and a<b)?
+  // equals to a - m + b - m <= 0
+  if (a + b - 2 * m >= 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
 // @lc code=end
 
-let arr = [1,2,3,4,5,5,8,9]
+let arr = [1, 2, 3, 4, 5, 5, 8, 9]
 
-console.log(findClosestElements(arr,4,5));
+console.log(findClosestElements(arr, 4, 5));
